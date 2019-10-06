2c9de4e4f8deca00c3058455ca464d066536a01c
Hartmann <alemaohartmann@gmail.com>
2019-10-05 19:13:48 -0500

    Tela de login

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,99 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
-      <v-icon class="mx-4" color="yellow">mdi-corn</v-icon>
-      <v-toolbar-title class="mr-12 align-center">Project-One</v-toolbar-title>
-    </v-app-bar>
-
     <v-content>
       <v-container
         class="fill-height"
+        id="container-app"
         fluid
       >
-        <v-row
-          align="center"
-          justify="center"
+        <v-col
+          cols="12"
+          md="4"
+          lg="4"
+          xl="4"
         >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
+          <v-row class="justify-center justify-md-start">
+            <v-img
+              src="./assets/logo.png"
+              id="img-logo"
+              aspect-ratio="1"
+            />
+          </v-row>
+          <v-toolbar
+            color="white"
+            dark
+            flat
           >
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="indigo"
-                dark
-                flat
-              >
-                <v-toolbar-title>LOGIN</v-toolbar-title>
-                <div class="flex-grow-1"></div>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      :href="source"
-                      icon
-                      large
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-account-plus</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Create new account</span>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                  v-model="login"
-                    label="Login"
-                    name="login"
-                    type="text"
-                    counter="50"
-                  ></v-text-field>
-
-                  <v-text-field
+            <v-toolbar-title 
+              class="headline font-weight-bold black--text" 
+            >
+              LOGIN
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                outlined
+                v-model="login"
+                label="NOME DE USUÁRIO"
+                name="login"
+                type="text"
+              />
+              <v-text-field
+                    outlined
                     v-model="password"
-                    label="Password"
+                    label="SENHA"
                     name="password"
                     type="password"
-                    counter="50"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-alert v-if="error" class="error" style="text-align: center;">Algo está errado!!!</v-alert>
-              <v-card-actions>
-                <div class="flex-grow-1"></div>
-                <v-btn color="secondary" @click="checkForm">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
+              />
+            </v-form>
+          </v-card-text>
+          <v-alert
+            v-if="errorlogin" 
+            type="error"
+          >
+            <v-row align="center">
+              Suas Credenciais de Login não coincidem com uma conta em nosso sistema
+            </v-row>
+          </v-alert>
+          <v-alert
+            v-if="errorblank" 
+            type="error"
+          >
+            <v-row align="center">
+              Deve ter pelo menos 2 caracteres
+            </v-row>
+          </v-alert>
+          <v-card-actions class="justify-center">
+            <v-btn
+              id="btn-login"
+              fab
+              color="#C62828"
+              large
+              @click="checkForm"
+            >
+              <v-icon color="#fff">mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+        <v-col
+          cols="8"
+          class="d-none d-md-flex"
+          id="col-img-login"
+        >
+          <v-img
+            id="img-bg-login"
+            src="./assets/Trophy_1920x1080.jpg"
+          />
+        </v-col>
       </v-container>
     </v-content>
-    <v-footer app color="secondary">
-      <span class="white--text">&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
-import Timeline from './data/db.json';
+  import Timeline from './data/db.json';
+  require('./assets/style.css');
 
   export default {
     props: {
@@ -92,24 +103,26 @@ import Timeline from './data/db.json';
       return {
       login: null,
       password: null,
-      error: false,
+      errorlogin: false,
+      errorblank: false,
       };
     },
     methods: {
       checkForm() {
-        if ((this.login && this.login.length <= 50) && (this.password && this.password.length <= 50)) {
+        this.errorlogin = false;
+        this.errorblank = false;
+        if ((this.login && this.login.length >= 2) && (this.password && this.password.length >=2)) {
           for(var i=0; i < Timeline.length; i++){
             if(this.login == Timeline[i].login && this.password == Timeline[i].senha){
               break;
             }
             else{
-              this.error = true;
+              this.errorlogin = true;
               break;
             }
           }
-          
-        }else{
-          this.error = true;
+        } else {
+          this.errorblank = true;
         }
       }
     },
